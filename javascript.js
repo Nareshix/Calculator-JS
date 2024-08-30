7dad541d2c36d3bf2cd8219d22092b94681c196f
nareshix <nareshix66@gmail.com>
2024-08-30 18:56:05 +0800

    displays number when clicked

--- javascript.js
+++ javascript.js
@@ -17,3 +17,43 @@ const operation = (num1,num2, operator) => {
             return divide(num1,num2);
     }
 }
+
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.textContent) {
+            case '1':
+                display.textContent += '1';
+                break;
+            case '2':
+                display.textContent += '2';
+                break;
+            case '3':
+                display.textContent += '3';
+                break;
+            case '4':
+                display.textContent += '4';
+                break;
+            case '5':
+                display.textContent += '5';
+                break;
+            case '6':
+                display.textContent += '6';
+                break;
+            case '7':
+                display.textContent += '7';
+                break;
+            case '8':
+                display.textContent += '8';
+                break;
+            case '9':
+                display.textContent += '9';
+                break;
+            case '0':
+                display.textContent += '0';
+                break;
+        }
+    })
+});
